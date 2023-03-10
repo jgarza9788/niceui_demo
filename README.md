30d780c5b5f04b457fc979a065a8ae215e548c56
jgarza9788 <jgarza9788@gmail.com>
2023-03-10 00:17:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# niceui_demo
+ 
---
+++ niceui_00.py
@@ -0,0 +1,24 @@
+from nicegui import ui
+from nicegui.events import ValueChangeEventArguments
+
+b = True
+
+def show(event: ValueChangeEventArguments):
+    name = type(event.sender).__name__
+    ui.notify(f'{name}: {event.value}')
+    b != b
+    
+
+ui.button('Button', on_click=lambda: ui.notify('Click'))
+with ui.row():
+    ui.checkbox('Checkbox', on_change=show)
+    ui.switch('Switch', on_change=show)
+ui.radio(['A', 'B', 'C'], value='A', on_change=show).props('inline')
+with ui.row():
+    ui.input('Text input', on_change=show)
+    if b:
+        ui.select(['One', 'Two'], value='One', on_change=show)
+
+ui.link('And many more...', '/reference').classes('mt-8')
+
+ui.run()
